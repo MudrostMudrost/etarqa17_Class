abf65aaa5cb532fc0cbad099197f87304856dbbd
MudrostMudrost <46790660+MudrostMudrost@users.noreply.github.com>
2019-02-16 19:23:49 +0200

    readme

--- second-selenium-project/src/test/java/tests/TestBase.java
+++ second-selenium-project/src/test/java/tests/TestBase.java
@@ -3,7 +3,7 @@ package tests;
 import manager.ApplicationManager;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-
+//readme
 public class TestBase {
 
   protected static ApplicationManager app = new ApplicationManager();
